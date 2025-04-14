fc5d017537443793675bff162a07763234392dfd
Dmitrii112233 <dmitriiberezovskiy@gmail.com>
2025-04-14 11:35:17 +0300

    Create array_methods_task6.js

---
+++ array_methods_task6.js
@@ -0,0 +1,8 @@
+let colors = ["white", "blue", "yellow", "black", "red", "green"];
+
+// destructuring
+let [firstColor, secondColor, ...otherColors] = colors;
+
+console.log(firstColor);
+console.log(secondColor);
+console.log(otherColors); 
